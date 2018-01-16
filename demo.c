7bf4926a416de1419286dd82810b0f010c42a4db
Felix Schlösser <felix@belmail.net>
2018-01-16 10:34:04 +0100

    ball now bounces off the sides

--- demo.c
+++ demo.c
@@ -2,13 +2,37 @@
 #include <ncurses.h>
 #include <unistd.h>
 
+#define DELAY 20000 // susped execution for microsecond intervals
+
 int main(int argc, char *argv[]) {
+  int x = 0, y = 0;
+  int max_y, max_x = 0;
+  int next_x = 0;
+  int direction = 1;
 
- initscr();
- noecho();
- curs_set(FALSE);
+ initscr(); // intitalize the window
+ noecho(); // don't echo keypresses
+ curs_set(FALSE); // don't display a cursor
 
- sleep(1);
+
+
+ while(1) {
+   // global var "stdscr" is created by the call to "intitscr()"
+   getmaxyx(stdscr, max_y, max_x);
+   clear(); // clear the screen of all prev. printed characters
+   mvprintw(y, x, "o"); // Print our "ball" at the current xy position
+   refresh();
+
+   usleep(DELAY); // Shorter delay between movements.
+
+   next_x = x + direction; // advance ball in dircetion
+
+   if (next_x >= max_x ||  next_x < 0) {
+     direction *= -1;
+   } else {
+     x += direction;
+   }
+ }
 
  endwin(); // Restore normal terminal behavior
 }
