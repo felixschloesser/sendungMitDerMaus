ec49ba93d86dab2eb80373e7ea1d5e9c2c6f0eb1
Felix Schlösser <felix.schloesser@tuhh.de>
2018-01-24 12:38:07 +0100

    for erich

--- maus/maus.c
+++ maus/maus.c
@@ -6,47 +6,73 @@
 
 #define DELAY 80000 // susped execution for microsecond intervals
 
-int main(int argc, char *argv[]) {
-  int max_y, max_x = 0;
-  int next_x = 0;
+int max_y, max_x = 0;
 
-  int maus_pos = 1;
 
+
+struct sprite {
+  int x;
+  int y;
+} rope;
+
+// struct character {
+//     int pos; // 1 for right -1 for left
+// } mouse;
+
+void init() {
   initscr(); // intitalize the window
   noecho(); // don't echo keypresses
   curs_set(FALSE); // don't display a cursor
   getmaxyx(stdscr, max_y, max_x);
   // global var "stdscr" is created by the call to "intitscr()"
-
-  struct sprite {
-    int x;
-    int y;
-  } rope;
-
-  struct character {
-      pos = 1; // 1 for right -1 for left
-  } mouse;
-
-void init() {
-  rope.x = (max_x / 2);
-  rope. y = 0;
-
-  mouse.pos = 1;
 }
 
+void init_sprites() {
+  rope.x = (max_x / 2);
+  rope.y = 0;
+  // mouse.pos = 1;
+}
 
- while(1) {
+void print_mouse(int y, int x, int orientation) {
+  char mouse[5] = {'@','#','#','~','\0'};
+  int i = 0;
 
-   // clear(); // clear the screen of all prev. printed characters
-   mvprintw(rope.y, rope.x, "#"); // Print our "ball" at the current xy position
-   refresh();
-   usleep(DELAY); // Shorter delay between movements.
+  mvprintw(y, x, "@##~");
+}
 
-   init();
+int main(int argc, char *argv[]) {
 
-   rope.y += 1;
+  init();
+  init_sprites();
 
-   endwin(); // Restore normal terminal behavior
+  int i,j = 0;
 
-   return 0;
+  // DRAW THE FLOOR
+  while(i < max_x) {
+    mvprintw(max_y-1, i, "=");
+    i += 1;
+  }
+
+  // DROPPING THE ROPE
+  while(j < max_y-1) {
+    // clear(); // clear the screen of all prev. printed characters
+    mvprintw(rope.y, rope.x, "#"); // Print the rope
+    refresh();
+    usleep(DELAY); // Shorter delay between movements.
+
+    rope.y += 1;
+    j++;
+  }
+
+  i = 0;
+
+  //print_mouse(max_y-4, max_x-2, 0);
+  mvprintw(0, 0, "@##~");
+
+  getchar();  // Press any button to exit
+
+  endwin(); // Restore normal terminal behavior
+
+  return 0;
+
 }
